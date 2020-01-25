af7f685dd58b4b0a11c84bec07d88471f4e19fa8
xuxue123 <1324748556@qq.com>
2020-01-25 11:22:00 +0800

    feat:使用springboot运行helloword

---
+++ src/main/java/com/example/springboothelloword/Controller/helloword.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloword.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class helloword {
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "hello word!";
+    }
+}
---
+++ src/main/java/com/example/springboothelloword/SpringbootHellowordApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboothelloword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHellowordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHellowordApplication.class, args);
+    }
+
+}
